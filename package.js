8abd151c8f589ec781b1a960afb313c47766abdb
ShMcK <me@shmck.com>
2015-07-19 19:08:03 +0900

    module loader updates

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "shmck:angular2",
-  summary: "Sets up the latest version of Angular 2",
+  summary: "Angular 2 packaged for Meteor. Alpha-31.",
   version: "2.0.0-alpha.31",
   git: "https://github.com/ShMcK/meteor-angular2",
   documentation: "README.md"
@@ -15,13 +15,13 @@ Package.registerBuildPlugin({
 
 Package.onUse(function (api) {
   api.versionsFrom("1.0.1");
-  api.use(["meteor", "ddp", "jquery"]); //
+  api.use(["meteor", "ddp"]); //jquery
 
   api.addFiles([
-    'lib/traceur-runtime.js',
-    'lib/es6-module-loader@0.16.5.js',
-    'lib/system@0.16.7.js',
-    "lib/angular@2.0.0-alpha.31.dev.js" //
+    'lib/traceur-runtime.min.js',
+    'lib/es6-module-loader.js',
+    'lib/system.js',
+    "lib/angular@2.0.0-alpha.31.dev.js"
     // contains angular, reflect-metadata, rtts_assert, rx, zone.js, System.config
     ], ["client"]);
 });
